5d3d72dc01165e55ed5220371264e811e9763c5d
omersuve <omersuve@gmail.com>
2024-08-27 06:19:54 +0300

    default host localhost

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  HOST: process.env.DB_HOST,
+  HOST: process.env.DB_HOST || "localhost",
   USER: process.env.DB_USERNAME,
   PASSWORD: process.env.DB_PASS,
   DB: process.env.DB_NAME,
